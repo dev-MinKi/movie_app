b1efb4af0958dca10aed4f0538ca54d488bdbfcb
kimminki <alsrl8755@naver.com>
2020-08-25 14:05:08 +0900

    Updates

---
+++ precache-manifest.cde1a7f40a0943d1436786df510ac98c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf2616b2d15b7f830666ca2c938c5cb",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f0e05169e3bd38137cf2",
+    "url": "/movie_app/static/css/main.a1188a97.chunk.css"
+  },
+  {
+    "revision": "429711d145589973fd96",
+    "url": "/movie_app/static/js/2.08856006.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/movie_app/static/js/2.08856006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e05169e3bd38137cf2",
+    "url": "/movie_app/static/js/main.c6009aa5.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
